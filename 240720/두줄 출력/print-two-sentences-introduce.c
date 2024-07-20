948b7aa218e613470d8766288c5968f4669f982e
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2024-07-20 16:44:47 +0900

    Title: 두줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240720/두줄 출력/print-two-sentences-introduce.c
+++ 240720/두줄 출력/print-two-sentences-introduce.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello students!\nWelcome to LeevrosCode!");// 여기에 코드를 작성해주세요.
+    printf("Hello students!\nWelcome to LeebrosCode!");// 여기에 코드를 작성해주세요.
     return 0;
 }
