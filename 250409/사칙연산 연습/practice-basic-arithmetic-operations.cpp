ec107a5751b315d45c012aef23d5734386148ddb
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2025-04-09 14:22:07 +0900

    Title: 사칙연산 연습, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+    int c_1 = c / 8;
+    int c_2 = c % 8;
+
+    cout<<c_1<<endl<<c_2<<endl;
+    return 0;
+}
