07c9441a894708e0a7c59227565c601c6c57bf90
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2025-04-09 16:21:07 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, num3;
+
+    cin>>num1>>num2>>num3;
+
+    cout<<fixed;
+    cout.precision(3);
+
+    cout<<num1<<endl<<num2<<endl<<num3;
+    return 0;
+}
