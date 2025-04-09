0b7cb953984f087971dbb8d3af8b67b33866a40f
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2025-04-09 16:43:03 +0900

    Title: 음수 구별하기, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1;
+
+    cin>>num1;
+    cout<<num1<<endl;
+
+    if(num1 < 0) {
+        cout<<"minus";
+    }
+
+
+    return 0;
+}
