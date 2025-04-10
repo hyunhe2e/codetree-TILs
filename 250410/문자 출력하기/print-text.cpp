b616fb4f862785fbe1f0fadc02bbd4bd4d293b31
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2025-04-10 09:29:35 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char saying;
+
+    cin>>saying;
+
+    for(int i = 0; i < 8; i++) {
+        cout<<saying;
+    }    
+    return 0;
+}
