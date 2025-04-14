3aa19d0f08d0138a6d1a1a3f57c13798bbf98533
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2025-04-15 01:07:41 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250415/친근하지 않은 수/unfriendly-number.cpp
+++ 250415/친근하지 않은 수/unfriendly-number.cpp
@@ -8,7 +8,7 @@ int main() {
 
     cin>>N;
 
-    for(int i = 0; i < N; i++) {
+    for(int i = 1; i <= N; i++) {
         if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0){
             continue;
         }
