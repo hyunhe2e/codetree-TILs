56a48788a6d653a8d627138686f359329a44d6f0
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2025-04-28 19:51:28 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250428/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int weight = 13;
+    double ratio = 0.165;
+
+    cout << fixed;
+    cout<<weight<<" * "<<ratio<<" = "<<weight*ratio;
+
+    return 0;
+}
