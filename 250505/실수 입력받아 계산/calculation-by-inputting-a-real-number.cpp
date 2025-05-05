7c828a8db055245ed9aa65ca3627c37b3ff9da07
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2025-05-05 23:57:34 +0900

    Title: 실수 입력받아 계산, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250505/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+
+    cin>>a>>b;
+
+    cout<<fixed;
+    cout.precision(2);
+
+    sum = a + b;
+
+    cout<<sum;
+    return 0;
+}
