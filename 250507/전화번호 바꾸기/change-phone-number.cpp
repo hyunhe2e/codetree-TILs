7b3586b702edc40b16f90771ac0064fc8a080111
Junghyun Kim <119275894+hyunhe2e@users.noreply.github.com>
2025-05-07 00:16:22 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250507/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w, x, y;
+
+    cin >> w;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+    cin.get();
+
+    cout << "010" << "-" << y << "-" << x; 
+    return 0;
+}
